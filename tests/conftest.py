85399b5e64fda977154b3be56ed216d92525cfa4
Stephen Lowery <stephen.e.lowery@gmail.com>
2021-01-16 13:57:55 -0500

    +pytest config

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+""" Pytest configuration """
+
+import pytest
+
---
+++ tests/test_tests.py
@@ -0,0 +1,6 @@
+""" Assert tests are able to execute """
+
+
+def test_tests():
+    """ Assert tests are able to execute """
+    assert True
